6cf6db1e04e783f60b0d552b37cbb87872f2148c
Heorhii <120305554+foxfollow@users.noreply.github.com>
2026-02-16 16:32:10 +0200

    ciflow: bump stone-clipboarder-tool to v1.4.0

--- Casks/stone-clipboarder-tool.rb
+++ Casks/stone-clipboarder-tool.rb
@@ -1,6 +1,6 @@
 cask "stone-clipboarder-tool" do
-  version "1.3.2"
-  sha256 "b2aa0c9cd5e956b915639fa0ccad61ddb8b172ee3a62214c4a2a542d1c44f393"
+  version "1.4.0"
+  sha256 "bc00ac9d6b4b0b4c0d760d20fe13068f23872a797b2d9badca853ae1b8c9d51b"
   
   url "https://github.com/foxfollow/Stone-Clipboarder-Tool/releases/download/v#{version}/StoneClipboarderTool.zip"
   name "Stone Clipboarder Tool"
